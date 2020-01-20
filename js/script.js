151c59e8d26a3c9bf0d1be299a680450fa8beb36
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-01-20 14:13:07 +0300

    Выполнил практическое задание из 3-его урока

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
+// Переделайте makeGETRequest() так, чтобы она использовала промисы.
+// Добавьте в соответствующие классы методы добавления товара в корзину, удаления товара из корзины и получения списка товаров корзины.
+
 'use strict';
 
 class GoodsItem {
   constructor( title, price, img = 'img/default-item.jpg') {
-    this.title = title;
+    this.product_name = title;
     this.price = price;
     this.img = img;
   }
   render() {
-    return `<div class="goods-item"><h3>${this.title}</h3>
+    return `<div class="goods-item"><h3>${this.product_name}</h3>
     <img src=${this.img}></img>
     <p>${this.price} <span class="rub">&#8399;</span></p>
     <button class="cart-button" type="button">Купить</button>
@@ -19,18 +22,17 @@ class GoodsList {
   constructor() {
     this.goods = [];
   }
-  fetchGoods() {
-    this.goods = [
-      { title: 'Shirt', price: 150},
-      { title: 'Socks', price: 50},
-      { title: 'Jacket', price: 350},
-      { title: 'Shoes', price: 250},
-    ];
+  fetchGoods(cb) {
+    makeGETRequest(`${API_URL}/catalogData.json`, (goods) =>{
+      this.goods = JSON.parse(goods);
+      cb();
+    })
   }
+
   render() {
     let listHtml = '';
     this.goods.forEach( good => {
-      const goodItem = new GoodsItem(good.title, good.price, good.img);
+      const goodItem = new GoodsItem(good.product_name, good.price, good.img);
       listHtml += goodItem.render();
     });
     document.querySelector('.goods-list').insertAdjacentHTML('afterBegin', listHtml);
@@ -45,25 +47,31 @@ class GoodsList {
   }
 }
 
-// Добавьте пустые классы для Корзины товаров и Элемента корзины товаров. Продумайте, какие методы понадобятся для работы с этими сущностями.
-class BasketList {
-  sumBasket() {
-    // Сумма товаров в корзине
+function makeGETRequest(url, callback) {
+  var xhr;
+
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
   }
-  delItem() {
-    // Удалить товар из корзины
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      callback(xhr.responseText);
+    }
   }
-  quantityItem() {
-    // Изменение количества товара в заказе
-  }
+
+  xhr.open('GET', url, true);
+  xhr.send();
 }
 
-class BasketItem {
-}
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 const list = new GoodsList();
-list.fetchGoods();
-list.render();
+list.fetchGoods(() => {
+  list.render();
+});
 list.sumGoods();
 
 
