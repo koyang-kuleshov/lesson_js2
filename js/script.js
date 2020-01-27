1c9099cf7eb4eff876922a33348f246d6aceee27
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-01-28 01:41:13 +0300

    Перевёл проект на Vue. Нужно сделать корзину

--- js/script.js
+++ js/script.js
@@ -2,105 +2,44 @@
 
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-class GoodsItem {
-  constructor( title, price, img = 'img/default-item.jpg') {
-    this.product_name = title;
-    this.price = price;
-    this.img = img;
-  }
-  render() {
-    return `<div class="goods-item"><h3>${this.product_name}</h3>
-    <img src=${this.img}></img>
-    <p>${this.price} <span class="rub">&#8399;</span></p>
-    <button class="cart-button" type="button">Купить</button>
-    </div>`;
-  }
-}
-
-class GoodsList {
-  constructor() {
-    this.goods = [];
-  }
-  fetchGoods(cb) {
-    makeGETRequest(`${API_URL}/catalogData.json`, (goods) =>{
-      this.goods = [...goods];
-      cb();
-    })
-  }
-
-// Метод, который определяет суммарную стоимость всех товаров.
-  sumGoods() {
-    let summ = 0;
-    this.goods.forEach( good => {
-      summ += parseInt(good.price);
-    });
-    return summ;
-  }
-}
-
-function makeGETRequest(url, callback) {
-  fetch(url)
-    .then(result => result.json())
-    .then(data => {
-      let listHtml = '';
-      data.forEach( good => {
-        const goodItem = new GoodsItem(good.product_name, good.price, good.img);
-        listHtml += goodItem.render();
-      });
-      document.querySelector('.goods-list').insertAdjacentHTML('afterBegin', listHtml);
-    })
-    .catch(console.log( `Данные не получены` ))
-}
-
-let makePOSTRequest = (url) => {
-// Сделать функцию для POST запроса
-  return new Promise((resolve, reject) => {
-    let xhr;
-    if (window.XMLHttpRequest) {
-      xhr = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-      xhr = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xhr.open('POST', url, true);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === '200') {
-          resolve(xhr.responseText);
-        } else {
-          reject('Error')
+const app = new Vue({
+  el: '#app',
+  data: {
+    goods: [],
+    filteredGoods: [],
+    catalogUrl: '/catalogData.json',
+    productImg: 'img/default-item.jpg',
+    searchLine: '',
+    basket: [],
+    isVisibleCart: false,
+    },
+  methods: {
+    fetchGoods(url) {
+      return fetch(url)
+        .then(result => result.json())
+        .catch(error => {
+          console.log(`Ошибка: ${error}`);
+        });
+    },
+    addProduct(good) {
+      this.basket.push(good);
+    },
+    FilterGoods() {
+      const regexp = new RegExp(this.searchLine, 'i');
+      this.filteredGoods = this.goods.filter(good =>
+        regexp.test(good.product_name));
+    },
+    },
+  computed: {
+  },
+  mounted() {
+    this.fetchGoods(`${API_URL + this.catalogUrl}`)
+      .then( data => {
+        for (let good of data) {
+          this.goods.push(good);
         }
-      }
-    }
-  xhr.send();
-  });
-}
-
-// Добавьте в соответствующие классы методы добавления товара в корзину, удаления товара из корзины и получения списка товаров корзины.
-
-class BasketList {
-
-  getBasket() {
-  // /getBasket.json - получить содержимое корзины
-    makeGETRequest(`${API_URL}/getBasket.json`, (goods) =>{
-      this.goods = JSON.parse(goods);
-    })
-  }
-
-  addToBasket(good) {
-    makePOSTRequest(`${API_URL}/addToBasket.json`, (good) =>{
-      this.good = JSON.stringify(good);
-    })
-  }
-
-  delItem() {
-    // Удалить товар из корзины
-    makePOSTRequest(`${API_URL}/deleteFromBasket.json`, (good) =>{
-      this.good = JSON.stringify(good);
-    })
-  }
-}
-
-const list = new GoodsList();
-list.fetchGoods();
-list.sumGoods();
+        this.filteredGoods = this.goods;
+      })
+  },
+});
 
