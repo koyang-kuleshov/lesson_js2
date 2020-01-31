0f6b6a4e78d80bc6a3399fc48e8ddeaee5d25ccf
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-02-01 01:04:45 +0300

    изменено:      public/js/CartComp.js

--- public/js/CartComp.js
+++ public/js/CartComp.js
@@ -53,10 +53,10 @@ Vue.component('cart', {
             <button class="btn-cart" type="button" @click="showCart = !showCart">Корзина</button>
             <div class="cart-block" v-show="showCart">
                 <p v-if="!cartItems.length">Корзина пуста</p>
-                <cart-item class="cart-item" 
-                v-for="item of cartItems" 
+                <cart-item class="cart-item"
+                v-for="item of cartItems"
                 :key="item.id_product"
-                :cart-item="item" 
+                :cart-item="item"
                 :img="imgCart"
                 @remove="remove">
                 </cart-item>
